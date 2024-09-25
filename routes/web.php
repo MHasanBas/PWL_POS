b8167619f15c7539147d7faa6e360aa4297a79ce
MHasanBas <hasanbasr054@gmail.com>
2024-09-25 17:24:42 +0700

    Jobsheet 5 - praktikum 1 dan 2

--- routes/web.php
+++ routes/web.php
@@ -3,18 +3,42 @@
 use App\Http\Controllers\KategoriController;
 use App\Http\Controllers\LevelController;
 use App\Http\Controllers\UserController;
+use App\Http\Controllers\WelcomeController;
 use Illuminate\Support\Facades\Route;
 
+// Keep this route to point to the WelcomeController
+Route::get('/', [WelcomeController::class, 'index']);
+
+// User routes
 Route::get('/user/tambah', [UserController::class, 'tambah']);
 Route::put('user/ubah_simpan/{id}', [UserController::class, 'ubah_simpan']);
 Route::post('user/tambah_simpan', [UserController::class, 'tambah_simpan']);
 Route::get('user/ubah/{id}', [UserController::class, 'ubah']);
 Route::get('/user/hapus/{id}', [UserController::class, 'hapus']);
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
+// Other routes
 Route::get('/level', [LevelController::class, 'index']);
 Route::get('/kategori', [KategoriController::class, 'index']);
 Route::get('/user', [UserController::class, 'index']);
+
+
+// use App\Http\Controllers\KategoriController;
+// use App\Http\Controllers\LevelController;
+// use App\Http\Controllers\UserController;
+// use App\Http\Controllers\WelcomeController;
+// use Illuminate\Support\Facades\Route;
+
+// Route::get('/', [WelcomeController::class, 'index']);
+// Route::get('/user/tambah', [UserController::class, 'tambah']);
+// Route::put('user/ubah_simpan/{id}', [UserController::class, 'ubah_simpan']);
+// Route::post('user/tambah_simpan', [UserController::class, 'tambah_simpan']);
+// Route::get('user/ubah/{id}', [UserController::class, 'ubah']);
+// Route::get('/user/hapus/{id}', [UserController::class, 'hapus']);
+
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+// Route::get('/level', [LevelController::class, 'index']);
+// Route::get('/kategori', [KategoriController::class, 'index']);
+// Route::get('/user', [UserController::class, 'index']);
