f77733ccbbc5424e435ef11a7c6db4a76ddebc90
MHasanBas <hasanbasr054@gmail.com>
2024-11-06 11:15:46 +0700

    Jobsheet 10 - Praktikum 4

--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\LoginController;
 use App\Http\Controllers\Api\RegisterController;
+use App\Http\Controllers\Api\LevelController;
 
 /*
 |--------------------------------------------------------------------------
@@ -22,6 +23,13 @@ Route::post('/login', [LoginController::class, '__invoke'])->name('login');
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+// Route Level
+Route::get('levels', [LevelController::class, 'index']);
+Route::post('levels', [LevelController::class, 'store']);
+Route::get('levels/{level}', [LevelController::class, 'show']);
+Route::put('levels/{level}', [LevelController::class, 'update']);
+Route::delete('levels/{level}', [LevelController::class, 'destroy']);
 
 Route::post('/register', \App\Http\Controllers\Api\RegisterController::class) ->name('register');
 
